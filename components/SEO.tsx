bcdbd2595bfb230ec0a5c8a856d73d771bf9156f
William H <william.hermozo@protonmail.com>
2022-04-08 15:25:38 +0100

    feat: add SEO and favicon

---
+++ components/SEO.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import React from "react";
+
+export default function SEO() {
+  return (
+    <Head>
+      <title>william | frontend engineer</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="description"
+        content="William Hermozo, Frontend Engineer, UI/UX and humans."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="William Hzo - Frontend Engineer" />
+      <meta
+        property="og:description"
+        content="William Hzo - Frontend Engineer, UI/UX"
+      />
+      <meta property="og:locale" content="en_GB" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://williamhzo.me/" />
+      <meta name="theme-color" content="#151718" />
+    </Head>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,49 @@
 import type { NextPage } from "next";
 import Contact from "../components/Contact";
-import Footer from "../components/Contact";
 import Header from "../components/Header";
 import Hr from "../components/Hr";
 import Link from "../components/Link";
 import Section from "../components/Section";
+import SEO from "../components/SEO";
 import SubHeading from "../components/SubHeading";
 
 const Home: NextPage = () => {
   return (
-    <main className="mx-auto my-0 flex min-h-[100vh] max-w-[70ch] flex-col px-8 py-16 text-white lg:p-8 lg:pt-32">
-      <Header />
+    <>
+      <SEO />
 
-      <Hr />
+      <main className="mx-auto my-0 flex min-h-[100vh] max-w-[70ch] flex-col px-8 py-16 text-white lg:p-8 lg:pt-32">
+        <Header />
 
-      <Section>
-        <SubHeading>whodis</SubHeading>
+        <Hr />
 
-        <p>
-          Frontend Engineer with a deep interest in{" "}
-          <em className="font-serif">clean User Interfaces</em> and web
-          experiences, I thrive at the intersection of Product, Design and
-          Engineering.
-        </p>
+        <Section>
+          <SubHeading>whodis</SubHeading>
 
-        <p>
-          All things <em className="font-serif">Design Systems</em>, a11y and
-          JamStack.
-        </p>
+          <p>
+            Frontend Engineer with a deep interest in{" "}
+            <em className="font-serif">clean User Interfaces</em> and web
+            experiences, I thrive at the intersection of Product, Design and
+            Engineering.
+          </p>
 
-        <p>
-          Currently at <Link href="https://getmansa.com">Mansa</Link> building
-          the product from wireframes to QA, and{" "}
-          <em className="font-serif">learning a lot</em>.
-        </p>
-      </Section>
+          <p>
+            All things <em className="font-serif">Design Systems</em>, a11y and
+            JamStack.
+          </p>
 
-      <Hr />
+          <p>
+            Currently at <Link href="https://getmansa.com">Mansa</Link> building
+            the product from wireframes to QA, and{" "}
+            <em className="font-serif">learning a lot</em>.
+          </p>
+        </Section>
 
-      <Contact />
-    </main>
+        <Hr />
+
+        <Contact />
+      </main>
+    </>
   );
 };
 
