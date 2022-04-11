c6dd9ac53cdf09c2edbc475f894e3e7d09922695
William H <william.hermozo@protonmail.com>
2022-04-11 17:03:59 +0100

    feat: improve select text contast

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="mb-6 flex justify-between font-serif text-sm italic text-lowest-contrast">
-      <p>be kind, be humble</p>
+      <p>constantly learning</p>
 
       <p>Paris 2022</p>
     </footer>
